1c1ef6f27d18e642941638b97b7c645e4e3763a7
imalam64 <imalam64@gmail.com>
2018-11-07 11:37:04 -0800

    appleOrange.js is correct

--- WarmUp/appleOrange.js
+++ WarmUp/appleOrange.js
@@ -8,25 +8,24 @@ var a = 3;
 var b = 14;
 
 //location of apples or oranges from origin of tree
-var apples = [-2, 2 , 4, 6];
+var apples = [-2, 2 , 4, 20];
 var oranges = [-2, 3 , 7, -6];
 
 //If apple is negative, discard. If orange is positve, discard.
-//Fruit must fall within the range of 's' and 't'.
-
+//Fruit must fall from tree('a' or 'b') within the range of 's' and 't'.
 
 function countApplesAndOranges(s, t, a, b, apples, oranges) {
     let appleCount = 0;
     let orangeCount = 0;
 
     for (let i=0; i < apples.length; i++){
-        if(apples[i] > 0 /* &&  */){
+        if(apples[i] > 0 && (apples[i] + a) >= s && (apples[i] + a)<= t){
             appleCount++;
         }
     }
 
     for (let i=0; i < oranges.length; i++){
-        if(oranges[i] < 0 /* &&  */){
+        if(oranges[i] < 0 && (oranges[i] + b) >= s && (oranges[i] + b) <= t){
             orangeCount++;
         }
     }
