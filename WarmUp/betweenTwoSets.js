3ecb9ad2d2aef51573d83c2d8490d9203689dbb7
imalam64 <imalam64@gmail.com>
2018-11-12 12:31:44 -0800

    setup next problem

---
+++ WarmUp/betweenTwoSets.js
@@ -0,0 +1,29 @@
+var arrA = [2, 4];
+var arrB = [16, 32, 96];
+
+// HackerRank did a terrible job explaining this problem conceptually. 
+//Reading the forum I found this explanation:
+// 1. Find LCM of the first array a. 
+// 2. Find GCD / HCF of the second array b. 
+// 3. Find all the multiples of LCM up to GCD, which divides the GCD evenly.
+
+
+/*
+ * Complete the getTotalX function below.
+ */
+function getTotalX(a, b) {
+    var LCM = null;
+    var GCD = null;
+    var b2Sets = null;
+
+    //use a loop to find the LCM of array A
+
+    //use a loop to find the GCD of array B
+
+    //use a loop to find all multiples
+
+    console.log(b2Sets);
+
+}
+
+getTotalX(arrA, arrB);
