c2c76f6918b3538f88bcc4d680e8968b1ed4f44c
imalam64 <imalam64@gmail.com>
2018-11-12 17:57:50 -0800

    next

--- WarmUp/betweenTwoSets.js
+++ WarmUp/betweenTwoSets.js
@@ -2,27 +2,47 @@ var arrA = [2, 4];
 var arrB = [16, 32, 96];
 
 // HackerRank did a terrible job explaining this problem conceptually. 
-//Reading the forum I found this explanation:
+// Reading the forum I found this explanation:
 // 1. Find LCM of the first array a. 
 // 2. Find GCD / HCF of the second array b. 
 // 3. Find all the multiples of LCM up to GCD, which divides the GCD evenly.
 
-
-/*
- * Complete the getTotalX function below.
- */
 function getTotalX(a, b) {
-    var LCM = null;
-    var GCD = null;
-    var b2Sets = null;
-
-    //use a loop to find the LCM of array A
-
-    //use a loop to find the GCD of array B
-
-    //use a loop to find all multiples
-
-    console.log(b2Sets);
+  // calculate aLast integer
+  const aLast = a[a.length - 1];
+  // calculate bFirst integer
+  const bFirst = b[0];
+  
+  // check which number(s) between aLast and bFist are divided evenly into and put into a new array
+  const arrTemp = [];
+  for(let i = aLast; i <= bFirst; i++) {
+    let flagA = true;
+    for(const elementA of a) {
+      if(i % elementA !== 0) {
+        flagA = false;
+      }
+    }
+    if(flagA) {
+      arrTemp.push(i);
+    }
+  }
+ 
+  // check which numbers divide new array elements evenly into and put into a result array
+  const arrResult = [];
+  for(let i=0; i < arrTemp.length; i++) {
+    let flagB = true;
+    for(const elementB of b) {
+      if(elementB % arrTemp[i] !== 0) {
+        flagB = false;
+      }
+    }
+    if(flagB) {
+      arrResult.push(arrTemp[i]);
+    }
+  }
+  
+  // return resultant array length
+  console.log(arrResult.length);
 
 }
 
---
+++ WarmUp/breakingRecord.js
@@ -0,0 +1,5 @@
+
+function breakingRecords(scores) {
+
+
+}
