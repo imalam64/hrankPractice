1a216367a1a65ed4de506fce3a313dc8f88b1616
imalam64 <imalam64@gmail.com>
2018-11-13 17:31:21 -0800

    partially working migBird

--- WarmUp/migBirds.js
+++ WarmUp/migBirds.js
@@ -1,21 +1,33 @@
-const birds = [1, 4, 4, 4, 5, 3];
+const birds = [1, 3, 3, 3, 4, 5, 5, 4, 3, 4, 5, 4, 5];
 
 function migratoryBirds(arr) {
-    let id = [], count = [];
+    let count = [0, 0, 0, 0, 0];
 
-    for (let i = 0; i < arr.length; i++){
-        if (id.indexOf(arr[i]) === -1) {
-            id.push(arr[i]);
-            id.push(arr[i][1] = 1)
-        } else if (id.indexOf(arr[i]) > -1){
-            id[id.indexOf(arr[i])]++
+    for (let i=0; i < arr.length; i++){
+        if(arr[i] == 1){
+            count[0]++;
+        } else if(arr[i] == 2){
+            count[1]++;
+        } else if(arr[i] == 3){
+            count[2]++;
+        } else if(arr[i] == 4){
+            count[3]++;
+        }else {
+            count[4]++;
         }
-
     }
 
-/*     for( let i=0; i< arr.)
- */
-    console.log(id);
+    let max = 0;
+    for(let i = 0; i< count.length; i++){
+        if (count[i] === count[i-1]){
+            max = max;
+        }
+        else if (count[i]> count[i-1]){
+            max = i+1
+        }
+    }
+
+    console.log(max);
 }
 
 migratoryBirds(birds);
